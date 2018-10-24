6e6adadcb39b3805554fa611f17a574180b9d256
Veraprot <vera.protopopova@gmail.com>
2018-10-24 13:03:58 -0400

    fix association

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
   has_many :reviews
+  has_many :user_movies
   has_many :movies, through: :reviews 
   has_many :movies, through: :user_movies 
-  def movie_list_count
-    self.reviews.select { |review| review.movie_id }.uniq.count 
-  end
 end
